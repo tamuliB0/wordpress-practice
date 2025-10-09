bf749284428e144e5e35ba55829bde10d6aa3b73
Bhardwaj Tamuli <bhardwajtamuli@gmail.com>
2025-10-09 19:48:57 +0530

    code update

--- mail-test/mail-test.php
+++ mail-test/mail-test.php
@@ -8,24 +8,42 @@ Author: ABC
 */
 
 
-register_activation_hook(__FILE__, 'mail_plugin_activate');
+// register_activation_hook(__FILE__, 'mail_plugin_activate');
 
-register_deactivation_hook(__FILE__, 'mail_plugin_deactivate');
+// register_deactivation_hook(__FILE__, 'mail_plugin_deactivate');
 
 
 
-function mail_plugin_activate() {
+// function mail_plugin_activate() {
+//     $email = get_option('admin_email');
+//     $subject = 'Plugin activated';
+//     $message = "Hello,".esc_html($email)." a mail plugin has been activated on your wordpress site" ;
+//     wp_mail($email, $subject, $message);
+// }
+
+// function mail_plugin_deactivate() {
+//     $email = get_option('admin_email');
+//     $subject = 'Plugin deactivated';
+//     $message = "Hello,".esc_html($email)." mail plugin has been deactivated on your wordpress site" ;
+//     wp_mail($email, $subject, $message);
+// }
+
+
+add_action('activated_plugin', 'activation_mail');
+
+function activation_mail($plugin) {
     $email = get_option('admin_email');
     $subject = 'Plugin activated';
-    $message = "Hello,".esc_html($email)." a mail plugin has been activated on your wordpress site" ;
+    $message = "The plugin". basename($plugin). "is activated." ;
     wp_mail($email, $subject, $message);
 }
 
-function mail_plugin_deactivate() {
+add_action('deactivated_plugin', 'deactivation_mail');
+
+function deactivation_mail($plugin) {
     $email = get_option('admin_email');
     $subject = 'Plugin deactivated';
-    $message = "Hello,".esc_html($email)." mail plugin has been deactivated on your wordpress site" ;
+    $message = "The plugin". basename($plugin). "is deactivated." ;
     wp_mail($email, $subject, $message);
 }
-
 
